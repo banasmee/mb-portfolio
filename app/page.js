1abd3fe117895afe569735dacf392cd5c05fcdcb
Martyna Banaś <banas.martynaa@gmail.com>
2024-02-09 10:39:07 +0100

    add website to portfolio

--- app/page.js
+++ app/page.js
@@ -32,8 +32,7 @@ export default function Home() {
           <div className="lg:ml-12 text-center lg:text-left pb-20">
             <h1 className={`text-4xl lg:text-6xl xl:text-7xl uppercase font-normal leading-normal font-second tracking-[0.305rem] mb-4 leading-10`}>Martyna Banaś</h1>
             <h2 className="text-2xl lg:text-3xl xl:text-[2.625rem] font-normal leading-normal uppercase">Front End Developer</h2>
-            <p className="text-md lg:text-xl max-w-xl mt-4">Cześć! Zajmuję się projektowaniem oraz&nbsp;programowaniem warstwy frontendowej aplikacji i&nbsp;stron internetowych.<br />
-            Aktualnie uczę się programowania w React i&nbsp;Next.js.</p>
+            <p className="text-md lg:text-xl max-w-xl mt-4">Cześć! Zajmuję się tworzeniem stron internetowych, a&nbsp;także ich projektowaniem. Mam kilka lat doświadczenia,&nbsp;ale wciąż się uczę -&nbsp;aktualnie poznaję frameworki React oraz&nbsp;Next</p>
           </div>
         </motion.div>
       </AnimatePresence>
